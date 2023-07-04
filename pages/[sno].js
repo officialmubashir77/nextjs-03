62b9a2feecff08e72ccb9ae894156bf152ca8c88
alinazir1 <alinazirportfolio@gmail.com>
2023-07-05 00:03:30 +0500

    03-file-base-routing

---
+++ pages/[sno].js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router';
+
+const Post = () => {
+
+    const router = useRouter() ; 
+    const { sno } = router.query ;
+    // console.log(sno);
+
+  return (
+    <p>Post: {sno}</p>
+  )
+}
+
+export default Post ;
---
+++ pages/components/about/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>Hello About Component</div>
+  )
+}
+
+export default about
